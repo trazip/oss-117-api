a349bb8347d905e0aa8273de4bc3b9b578741a78
Pierre Penhard <p.penhard@gmail.com>
2021-02-25 20:23:25 +0100

    Fixed models

--- db/migrate/20210225191901_create_authors.rb
+++ db/migrate/20210225191901_create_authors.rb
@@ -1,7 +1,8 @@
 class CreateAuthors < ActiveRecord::Migration[6.0]
   def change
     create_table :authors do |t|
-      t.string :name
+      t.string :fullname
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20210225191930_create_quotes.rb
+++ db/migrate/20210225191930_create_quotes.rb
@@ -1,8 +1,9 @@
 class CreateQuotes < ActiveRecord::Migration[6.0]
   def change
     create_table :quotes do |t|
-      t.text :content
+      t.string :content
       t.references :author, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
