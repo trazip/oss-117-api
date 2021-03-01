dcae3ae64c2d676da926518c9fc59a7024bb6923
Pierre Penhard <p.penhard@gmail.com>
2021-03-01 23:46:07 +0100

    Finished documentation

--- app/javascript/plugins/init_highlightjs.js
+++ app/javascript/plugins/init_highlightjs.js
@@ -1,4 +1,6 @@
 import hljs from 'highlight.js/lib/core';
 import 'highlight.js/styles/night-owl.css';
 import json from 'highlight.js/lib/languages/json';
+import curl from 'highlight.js/lib/languages/curl';
 hljs.registerLanguage('json', json);
+hljs.registerLanguage('curl', curl);
